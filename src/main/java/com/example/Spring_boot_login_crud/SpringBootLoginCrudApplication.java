3e9bad64fcd455efa45d3a4416fcecdf1314ac2e
Your Name <you@example.com>
2022-06-09 13:54:31 -0500

    se agregaron archivos principales de configuacion

---
+++ src/main/java/com/example/Spring_boot_login_crud/SpringBootLoginCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_boot_login_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoginCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLoginCrudApplication.class, args);
+	}
+
+}
